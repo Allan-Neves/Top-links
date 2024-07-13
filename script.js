bcc2acbf78c98fa77d438b3adaa2c6c14988852f
Brian Muniz Silveira <155079481+BrianMunizSilveira@users.noreply.github.com>
2024-07-13 00:10:15 -0300

    Modo escuro e mais algumas adições
    - Apliquei um modo escuro da aplicação trazendo mais conforto ao usuário que tem preferência ao modo escuro invés do modo claro;
    
    - Adicionei mais um link na seção de 'Cursos' - FreeCodeCamp;
    
    - Novos metadados visando a melhor acessibilidade á página;
    
    - Título mais especifico.

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const toggleButton = document.getElementById('toggle-dark-mode');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    // Armazene a preferência do usuário no localStorage
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
+
+// Verifique a preferência do usuário e aplique-a
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-mode');
+}
